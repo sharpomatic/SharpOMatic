99d080099421d4ca5f6f22c455badebef88a9eab
Phil Wright <phil.wright@componentfactory.com>
2025-12-06 20:54:05 +1100

    Type and Auth

--- src/Sharp-O-Matic.Editor/src/app/pages/connection/connection.component.ts
+++ src/Sharp-O-Matic.Editor/src/app/pages/connection/connection.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { finalize } from 'rxjs/operators';
 import { Connection } from '../../metadata/definitions/connection';
+import { ConnectionConfig } from '../../metadata/definitions/connection-config';
 import { ServerRepositoryService } from '../../services/server.repository.service';
+import { MetadataService } from '../../services/metadata.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -19,8 +20,10 @@ import { FormsModule } from '@angular/forms';
 export class ConnectionComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly serverRepository = inject(ServerRepositoryService);
+  private readonly metadataService = inject(MetadataService);
 
   public connection: Connection = new Connection(Connection.defaultSnapshot());
+  public connectionConfig: ConnectionConfig | null = null;
 
   ngOnInit(): void {
     const connectionId = this.route.snapshot.paramMap.get('id');
@@ -28,6 +31,7 @@ export class ConnectionComponent implements OnInit {
       this.serverRepository.getConnection(connectionId).subscribe(connection => {
         if (connection) {
           this.connection = connection;
+          this.loadConnectionConfig(connection.configId());
         }
       });
     }
@@ -38,5 +42,20 @@ export class ConnectionComponent implements OnInit {
       .subscribe(() => {
         this.connection?.markClean();
     });
-  }  
+  }
+
+  private loadConnectionConfig(configId: string): void {
+    if (!configId) {
+      this.connectionConfig = null;
+      return;
+    }
+
+    const configs = this.metadataService.connectionConfigs();
+    this.connectionConfig = configs.find(config => config.configId === configId) ?? null;
+
+    const authModes = this.connectionConfig?.authModes ?? [];
+    if (this.connection && !this.connection.authenticationModeId() && authModes.length > 0) {
+      this.connection.authenticationModeId.set(authModes[0].id);
+    }
+  }
 }
